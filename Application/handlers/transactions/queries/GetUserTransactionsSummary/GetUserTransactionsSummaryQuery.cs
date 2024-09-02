80c5866ce7ae276904456b04a7f4262e3b038ea1
Alaiv <akudrjavtsev@action.tech>
2024-09-02 21:19:15 +0300

    add missing file

---
+++ Application/handlers/transactions/queries/GetUserTransactionsSummary/GetUserTransactionsSummaryQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos;
+using Application.mediator.interfaces;
+
+namespace Application.handlers.transactions.queries.GetUserTransactionsSummary;
+
+public class GetUserTransactionsSummaryQuery : IRequest<List<TransactionSummaryDto>>
+{
+    public Guid UserId { get; set; }
+    
+    public Guid BudgetId { get; set; }
+}
